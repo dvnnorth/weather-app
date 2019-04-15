e5e956562005fcd235fa2406c376946b26575404
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2019-04-15 17:12:53 -0500

    Built basic application framework

--- weather_app.py
+++ weather_app.py
@@ -19,63 +19,77 @@ canvas_attr = {
 canvas = tk.Canvas(root, **canvas_attr)
 canvas.pack()
 
-# Frame definition
+# Top Frame definition
 frame_place = {
-  "relx": 0.1, # Percentage of screen offset on x (both sides)
+  "relx": 0.5, # Percentage of screen offset on x (both sides)
   "rely": 0.1, # Percentage of screen offset on y (both sides)
-  "relwidth": 0.8, # Percentage of screen size on x (both sides)
-  "relheight": 0.8 # Percentage of screen size on x (both sides)
+  "relwidth": 0.75, # Percentage of screen size on x (both sides)
+  "relheight": 0.1, # Percentage of screen size on x (both sides)
+  "anchor": "n"
 } # Note that 0.1 of the screen on both sides of x and y add to
   # 0.2 of the screen size and 0.8 + 0.2 = 1 (100% of screen)
   # These values scale with the size of the window (hence rel)
 frame_attr = {
-  "bg": "#80c1ff"
+  "bg": "#80c1ff",
+  "bd": 5
 }
 frame = tk.Frame(root, **frame_attr)
 frame.place(**frame_place)
+
+# Entry definition
+entry_attr = {
+  "font": 40
+}
+entry_place = {
+  "relwidth": 0.65,
+  "relheight": 1
+}
+entry = tk.Entry(frame, **entry_attr)
+entry.place(**entry_place)
 
 # Button definition
 # Button attributes dictionary
 button_attr = {
   "text": "Test Button",
-  "bg": "gray"
+  "font": 40
 }
 button_place = {
-  "relx": 0,
-  "rely": 0,
-  "relwidth": 0.25,
-  "relheight": 0.25
+  "relx": 0.7,
+  "relwidth": 0.3,
+  "relheight": 1
 }
 # Define button with dictionary unpacking (and keyword parameters)
 button = tk.Button(frame, **button_attr)
 button.place(**button_place)
 
+# Bottom Frame definition
+bottom_frame_place = {
+  "relx": 0.5, # Percentage of screen offset on x (both sides)
+  "rely": 0.25, # Percentage of screen offset on y (both sides)
+  "relwidth": 0.75, # Percentage of screen size on x (both sides)
+  "relheight": 0.6, # Percentage of screen size on x (both sides)
+  "anchor": "n"
+} # Note that 0.1 of the screen on both sides of x and y add to
+  # 0.2 of the screen size and 0.8 + 0.2 = 1 (100% of screen)
+  # These values scale with the size of the window (hence rel)
+bottom_frame_attr = {
+  "bg": "#80c1ff",
+  "bd": 10
+}
+bottom_frame = tk.Frame(root, **bottom_frame_attr)
+bottom_frame.place(**bottom_frame_place)
+
 # Label definition
 label_attr = {
   "text": "This is a label",
-  "bg": "yellow"
+  "bg": "white"
 }
 label_place = {
-  "relx": 0.3,
-  "rely": 0,
-  "relwidth": 0.45,
-  "relheight": 0.25
+  "relwidth": 1,
+  "relheight": 1
 }
-label = tk.Label(frame, **label_attr)
+label = tk.Label(bottom_frame, **label_attr)
 label.place(**label_place)
-
-# Entry definition
-entry_attr = {
-  "bg": "green"
-}
-entry_place = {
-  "relx": 0.8,
-  "rely": 0,
-  "relwidth": 0.2,
-  "relheight": 0.25
-}
-entry = tk.Entry(frame, **entry_attr)
-entry.place(**entry_place)
 
 # Execute main loop, bring up window
 root.mainloop()
